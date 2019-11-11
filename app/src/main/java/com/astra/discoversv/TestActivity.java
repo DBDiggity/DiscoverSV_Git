e9438e27ce3e0446ebf85b9e50f519aa354eccdb
Jason Alexis <mr.jason.alexis@hotmail.com>
2019-11-11 07:36:46 -0400

    Added Logo and Splash Screen

--- app/src/main/java/com/astra/discoversv/TestActivity.java
+++ app/src/main/java/com/astra/discoversv/TestActivity.java
@@ -33,6 +33,7 @@ public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
